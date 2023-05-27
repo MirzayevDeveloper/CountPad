78432da1ce70b096ff313f571d7dc95c80c9f6e1
MirzayevDeveloper <MirzayevDeveloper1@gmail.com>
2023-05-27 12:58:36 +0500

    Add Login Auth

---
+++ src/CountPad.Domain/Entities/Packages/Package.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CountPad Team
+// --------------------------------------------------------
+
+using System;
+using CountPad.Domain.Entities.Products;
+
+namespace CountPad.Domain.Entities.Packages
+{
+    public class Package : BaseAuditableEntity
+	{
+		public Double Count { get; set; }
+		public Decimal IncomingPrice { get; set; }
+		public Decimal SalePrice { get; set; }
+		public DateTime IncomingDate { get; set; }
+
+		public Guid ProductId { get; set; }
+		public Product Product { get; set; }
+
+		public Guid DistributorId { get; set; }
+		public Distributor Distributor { get; set; }
+	}
+}
