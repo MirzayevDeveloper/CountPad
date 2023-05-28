3dc01a2533172c544a4e0683607d5c570fef9697
MirzayevDeveloper <MirzayevDeveloper1@gmail.com>
2023-05-28 14:55:53 +0500

    cahnges

---
+++ src/CountPad.Application/UseCases/Packages/Queries/GetPackagesWithPagination/GetPackagesWithPaginationQuery.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using CountPad.Application.Common.Interfaces;
+using CountPad.Application.Common.Models;
+using CountPad.Application.UseCases.Packages.Models;
+using CountPad.Application.UseCases.Products.Models;
+using CountPad.Domain.Entities.Products;
+using MediatR;
+
+namespace CountPad.Application.UseCases.Packages.Queries.GetProductsWithPagination
+{
+	public class GetProductsWithPaginationQuery : IRequest<PaginatedList<PackageDto>>
+	{
+		public int PageNumber { get; init; } = 1;
+		public int PageSize { get; init; } = 10;
+	}
+
+	public class GetProductsWithPaginationQueryHandler : IRequestHandler<GetProductsWithPaginationQuery, PaginatedList<PackageDto>>
+	{
+		private readonly IApplicationDbContext _context;
+		private readonly IMapper _mapper;
+
+		public GetProductsWithPaginationQueryHandler(
+			IApplicationDbContext context,
+			IMapper mapper)
+		{
+			_context = context;
+			_mapper = mapper;
+		}
+
+		public async Task<PaginatedList<ProductDto>> Handle(GetProductsWithPaginationQuery request, CancellationToken cancellationToken)
+		{
+			Product[] products = _context.Products.ToArray();
+
+			IQueryable<ProductDto> dtos = _mapper.Map<ProductDto[]>(products).AsQueryable();
+
+			PaginatedList<ProductDto> paginatedList =
+				await PaginatedList<ProductDto>.CreateAsync(
+					dtos, request.PageNumber, request.PageSize);
+
+			return paginatedList;
+		}
+	}
+}
