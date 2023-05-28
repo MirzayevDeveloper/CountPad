27c60c9389e70f85e09779ebf3fcc4f7b6b76a47
MirzayevDeveloper <MirzayevDeveloper1@gmail.com>
2023-05-28 12:45:08 +0500

    Distributor controller added

---
+++ src/CountPad.Api/Controllers/DistributorsController.cs
@@ -0,0 +1,42 @@
+﻿using CountPad.Application.UseCases.Distributors.Commands.CreateDistributor;
+using CountPad.Application.UseCases.Distributors.Commands.DeleteDistributor;
+using CountPad.Application.UseCases.Distributors.Commands.UpdateDistributor;
+using CountPad.Application.UseCases.Distributors.Models;
+using CountPad.Application.UseCases.Distributors.Queries.GetDistributor;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CountPad.Api.Controllers
+{
+	public class DistributorsController : ApiControllerBase
+	{
+		[HttpPost]
+		public async ValueTask<ActionResult<DistributorDto>> PostDistributorAsync(CreateDistributorCommand command)
+		{
+			return await Mediator.Send(command);
+		}
+
+		[HttpGet("{distributorId}")]
+		public async ValueTask<ActionResult<DistributorDto>> GetDistributorAsync(Guid distributorId)
+		{
+			return await Mediator.Send(new GetDistributorQuery(distributorId));
+		}
+
+		[HttpGet]
+		public async ValueTask<ActionResult<DistributorDto[]>> GetAllDistributorsAsync()
+		{
+			return await Mediator.Send(new GetDistributorsQuery());
+		}
+
+		[HttpPut]
+		public async ValueTask<ActionResult<DistributorDto>> UpdateDistributorAsync(UpdateDistributorCommand command)
+		{
+			return await Mediator.Send(command);
+		}
+
+		[HttpDelete("{distributorId}")]
+		public async ValueTask<ActionResult<DistributorDto>> DeleteDistributorAsync(Guid distributorId)
+		{
+			return await Mediator.Send(new DeleteDistributorCommand(distributorId));
+		}
+	}
+}
