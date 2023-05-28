db9cfc02ae9069cf421f337c60821911f9ec5eeb
MirzayevDeveloper <MirzayevDeveloper1@gmail.com>
2023-05-28 14:32:04 +0500

    package updates

---
+++ src/CountPad.Application/UseCases/Packages/Commands/DeletePackage/DeletePackageCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountPad.Application.UseCases.Packages.Commands.DeletePackage
+{
+	internal class DeletePackageCommand
+	{
+	}
+}
