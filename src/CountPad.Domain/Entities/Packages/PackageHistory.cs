0acc1607884be9f808da68e3607df1ca5f235493
MirzayevDeveloper <MirzayevDeveloper1@gmail.com>
2023-05-29 14:16:36 +0500

    changes

---
+++ src/CountPad.Domain/Entities/Packages/PackageHistory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CountPad.Domain.Common.BaseEntities;
+using CountPad.Domain.Entities.Products;
+
+namespace CountPad.Domain.Entities.Packages
+{
+	public class PackageHistory : BaseAuditableEntity
+	{
+		public Double Count { get; set; }
+		public Decimal IncomingPrice { get; set; }
+		public Decimal SalePrice { get; set; }
+		public DateTimeOffset IncomingDate { get; set; }
+
+		public Guid ProductId { get; set; }
+		public Product Product { get; set; }
+
+		public Guid DistributorId { get; set; }
+		public Distributor Distributor { get; set; }
+	}
+}
